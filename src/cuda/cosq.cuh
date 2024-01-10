73eaf16d88759aaf1c4ed63a12ff3270119a5c95
JacobWongDev <19jsjw@queensu.ca>
2024-01-09 21:57:01 -0500

    Saving progress on implementing COSQ on CUDA - COSQ is executed on each 8x8 dct coefficient block - The codebook for each block (result of cosq) is stored as a sequence on each 8x8 block   in the matrix returned by the cosq kernel. - CUDA does not support variable length arrays. Hence, some of the arrays that should be   dynamic in the COSQ algorithm based on the target number of quantization levels are hardcoded at length BLOCK_SIZE2. - Since each quantizer is trained on BLOCK_SIZE2 values, the maximum number of quantization points that a quantizer   could use to represent all BLOCK_SIZE2 values is BLOCK_SIZE2 (in this case the distortion would be 0).   - This is why each codebook from each 8x8 block is returned on an 8x8 block instead of     dynamically allocating extra memory and returning the codebooks that way.   - It is also why the arrays in the COSQ algorithm are limited to size BLOCK_SIZE2 - Need to add support for bit allocation matrix.

---
+++ src/cuda/cosq.cuh
@@ -0,0 +1,218 @@
+#pragma once
+#include <device_launch_parameters.h>
+#include <cuda_runtime_api.h>
+#include "common.h"
+
+struct quantizer_cell {
+  struct quantizer_cell* next;
+  float* value;
+};
+
+typedef struct quantizer_cell cell;
+
+__device__ void nullify(cell** arr, int length) {
+  for(int i = 0; i < length; i++)
+    arr[i] = NULL;
+}
+
+/**
+ * @brief Error measure used by the cosq.
+ *
+ * @param a
+ * @param b
+ * @return float
+ */
+__device__ float error(float a, float b) {
+  return (a - b) * (a - b);
+}
+
+/**
+ * @brief Channel error probability for the Binary Symmetric Channel.
+ *
+ */
+__device__ float channel_error(int a, int b, int num_bits) {
+  // int x = a ^ b;
+  // int count = 0; // number of bits that differ
+  // float p = 0.2; // probability of error
+  // while (x) {
+  //   count += x & 1;
+  //   x >>= 1;
+  // }
+  // return pow(p, count) * pow(1-p, num_bits - count);
+  if(a == b)
+    return 1;
+  else
+    return 0;
+}
+
+/**
+ * @brief Generalized nearest neighbour condition.
+ *
+ * @param codebook
+ * @param regions
+ * @return float
+ */
+__device__ void nearest_neighbour(float* codebook, cell** roots, int levels, cell* regions, int training_size, int num_bits) {
+  float min = __FLT_MAX__;
+  int min_index = -1;
+  float sum = 0;
+  cell* previous[BLOCK_SIZE2]; // so we don't have to traverse the linked list every time we want to insert a new element
+  nullify(previous, levels);
+  for(int i = 0; i < training_size; i++) {
+    for(int l = 0; l < levels; l++) {
+      for(int j = 0; j < levels; j++) {
+        sum += channel_error(j, l, num_bits) * error(*(regions[i].value), codebook[j]);
+      }
+      if(sum < min) {
+        min_index = l;
+        min = sum;
+      }
+      sum=0;
+    }
+    // If first node in this partition:
+    if(previous[min_index] == NULL) {
+      roots[min_index] = &regions[i];
+      previous[min_index] = roots[min_index];
+    } else {
+      (*previous[min_index]).next = &regions[i];
+      previous[min_index] = &regions[i];
+    }
+    regions[i].next = NULL;
+    //reset
+    sum = 0;
+    min_index = -1;
+    min = __FLT_MAX__;
+  }
+}
+
+/**
+ * @brief Generalized centroid condition.
+ *
+ * @param regions
+ * @param codebook
+ * @return float
+ */
+__device__ void centroid(cell** roots, float* codebook, int levels, int num_bits) {
+  float numerator = 0;
+  float denominator = 0;
+  float partition_sum = 0;
+  float partition_sizes[BLOCK_SIZE2];
+  float partition_sums[BLOCK_SIZE2];
+  cell* ptr = NULL;
+  int count = 0;
+
+  // To save ourselves from calculating the same sum
+  // for each codebook value, calculate it once and save the value.
+  for(int i = 0; i < levels; i++) {
+    ptr = roots[i];
+    while(ptr != NULL) {
+      partition_sum += *((*ptr).value);
+      count++;
+      ptr = ptr->next;
+    }
+    partition_sizes[i] = count;
+    partition_sums[i] = partition_sum;
+    count = 0;
+    partition_sum = 0;
+  }
+
+  for(int i = 0; i < levels; i++) {
+    // Compute Numerator
+    for(int j = 0; j < levels; j++)
+      numerator += channel_error(i, j, num_bits) * partition_sums[j];
+    // Compute Denominator
+    for(int j = 0; j < levels; j++)
+      denominator += channel_error(i, j, num_bits) * partition_sizes[j];
+    codebook[i] = numerator/denominator;
+    numerator = 0;
+    denominator = 0;
+  }
+}
+
+/**
+ * @brief
+ *
+ * @param levels
+ * @param num_bits
+ * @param training
+ * @param codebook
+ * @return float
+ */
+__device__ float distortion(int levels, int num_bits, cell** roots, float* codebook) {
+  float d = 0;
+  cell* traversal = NULL;
+  for(int i = 0; i < levels; i++) {
+    traversal = roots[i];
+    while(traversal != NULL) {
+      for(int j = 0; j < levels; j++) {
+        d += channel_error(j, i, num_bits) * error(*(traversal->value), codebook[j]);
+      }
+      traversal = traversal->next;
+    }
+  }
+  return d / BLOCK_SIZE2;
+}
+
+
+__global__ void cosq(float* dst, float* src, int pitch, int num_bits, int levels) {
+  float current_distortion = 0;
+  float previous_distortion = 0;
+  float codebook[BLOCK_SIZE2];
+  cell* roots[BLOCK_SIZE2]; // roots is used to point to the beginning of the linked list.
+  nullify(roots, BLOCK_SIZE2);
+  cell regions[BLOCK_SIZE2]; // regions contain the actual values and pointers to next in list.
+  float threshold = 0.01;
+  src += blockIdx.y*pitch + BLOCK_SIZE*blockIdx.x;
+  dst += blockIdx.y*pitch + BLOCK_SIZE*blockIdx.x;
+  //setup regions
+  int k = 0;
+  for(int i = 0; i < BLOCK_SIZE; i++) {
+    for(int j = 0; j < BLOCK_SIZE; j++) {
+      regions[i * BLOCK_SIZE + j].value = &src[k++];
+      regions[i * BLOCK_SIZE + j].next = NULL;
+    }
+    k += pitch - BLOCK_SIZE;
+  }
+  // initialize codebook
+  // Use first N training points as initial codebook.
+  k = 0;
+  bool e = false;
+  for(int i = 0; i < BLOCK_SIZE; i++) {
+    for(int j = 0; j < BLOCK_SIZE; j++) {
+      if(i * BLOCK_SIZE + j < levels) {
+        codebook[i * BLOCK_SIZE + j] = src[k++];
+      } else {
+        e = true;
+        break;
+      }
+    }
+    if(e)
+      break;
+    k += pitch - BLOCK_SIZE;
+  }
+  // First iteration
+  nearest_neighbour(codebook, roots, levels, regions, BLOCK_SIZE2, num_bits);
+  centroid(roots, codebook, levels, num_bits);
+  previous_distortion = distortion(levels, num_bits, roots, codebook);
+  // Lloyd Iteration
+  while(1) {
+    nearest_neighbour(codebook, roots, levels, regions, BLOCK_SIZE2, num_bits);
+    centroid(roots, codebook, levels, num_bits);
+    current_distortion = distortion(levels, num_bits, roots, codebook);
+    if((previous_distortion - current_distortion) / previous_distortion < threshold)
+      break;
+    previous_distortion = current_distortion;
+  }
+  //save codebook to dst
+  k = 0;
+  for(int i = 0; i < BLOCK_SIZE; i++) {
+    for(int j = 0; j < BLOCK_SIZE; j++) {
+      if(i * BLOCK_SIZE + j < levels) {
+        dst[k++] = codebook[i * BLOCK_SIZE + j];
+      } else {
+        return;
+      }
+    }
+    k += pitch - BLOCK_SIZE;
+  }
+}
--- src/training/training.cu
+++ src/training/training.cu
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <vector_types.h>
+#include <math.h>
 #include "training.cuh"
 #include "../util/cuda_util.h"
 #include "../util/pgm_util.h"
 #include "../util/logger.h"
 #include "../cuda/dct.cuh"
+#include "../cuda/cosq.cuh"
 
 void center_pixels(float* pixels, int stride, int width, int height) {
     for(int i = 0; i < height; i++) {
@@ -23,7 +25,7 @@ int train(const char* dir_name, const char* channel_name) {
     DIR* directory = NULL;
     struct dirent* entry = NULL;
     float* matrix = NULL;
-    size_t device_stride;
+    size_t pitch;
     int matrix_stride;
     dim3 cuda_grid_size;
     dim3 cuda_block_size;
@@ -49,12 +51,17 @@ int train(const char* dir_name, const char* channel_name) {
         // Want pixel values to have mean 0, so subtract 128 from each.
         center_pixels(matrix, matrix_stride, image->width, image->height);
         // Allocate memory on GPU for grayscale matrices
-        checkCudaErrors(cudaMallocPitch((void **) &device_plane_src, &device_stride, image->width * sizeof(float), image->height));
-        checkCudaErrors(cudaMallocPitch((void **) &device_plane_result, &device_stride, image->width * sizeof(float), image->height));
-        device_stride /= sizeof(float);
+        checkCudaErrors(cudaMallocPitch((void **) &device_plane_src, &pitch, image->width * sizeof(float), image->height));
+        checkCudaErrors(cudaMallocPitch((void **) &device_plane_result, &pitch, image->width * sizeof(float), image->height));
+        pitch /= sizeof(float);
+
+        // Copy image matrix to GPU
         checkCudaErrors(cudaMemcpy2D(
-            device_plane_src, device_stride * sizeof(float), matrix, matrix_stride * sizeof(float),
+            device_plane_src, pitch * sizeof(float), matrix, matrix_stride * sizeof(float),
             image->width * sizeof(float), image->height, cudaMemcpyHostToDevice));
+        /*
+            DCT SETUP AND EXECUTION
+        */
         cuda_grid_size = {
             image->width / KER2_BLOCK_WIDTH,
             image->height / KER2_BLOCK_HEIGHT,
@@ -65,7 +72,7 @@ int train(const char* dir_name, const char* channel_name) {
             KER2_BLOCK_WIDTH / 8,
             KER2_BLOCK_HEIGHT / 8
         };
-        dct<<<cuda_grid_size, cuda_block_size>>>(device_plane_result, device_plane_src, (int) device_stride);
+        dct<<<cuda_grid_size, cuda_block_size>>>(device_plane_result, device_plane_src, (int) pitch);
 
         fprintf(stdout, "Matrix before DCT: \n");
         for(int i = 0; i < 8; i++) {
@@ -76,8 +83,9 @@ int train(const char* dir_name, const char* channel_name) {
             fprintf(stdout, "]\n");
         }
 
+        // Copy resulting matrix back to host
         checkCudaErrors(cudaMemcpy2D(
-            matrix, matrix_stride * sizeof(float), device_plane_result, device_stride * sizeof(float),
+            matrix, matrix_stride * sizeof(float), device_plane_result, pitch * sizeof(float),
             image->width * sizeof(float), image->height, cudaMemcpyDeviceToHost));
 
         fprintf(stdout, "Matrix after DCT: \n");
@@ -88,6 +96,37 @@ int train(const char* dir_name, const char* channel_name) {
             }
             fprintf(stdout, "]\n");
         }
+
+        /*
+            COSQ SETUP AND EXECUTION
+        */
+        // Swap pointers
+        float* tmp = device_plane_result;
+        device_plane_result = device_plane_src;
+        device_plane_src = tmp;
+
+        cuda_grid_size = {
+            image->width / BLOCK_SIZE,
+            image->height / BLOCK_SIZE,
+            1
+        };
+        cuda_block_size = {1, 1, 1};
+        int num_bits = 1;
+        cosq<<<cuda_grid_size, cuda_block_size>>>(device_plane_result, device_plane_src, (int) pitch, num_bits, 1 << num_bits);;
+        // Copy resulting matrix back to host
+        checkCudaErrors(cudaMemcpy2D(
+            matrix, matrix_stride * sizeof(float), device_plane_result, pitch * sizeof(float),
+            image->width * sizeof(float), image->height, cudaMemcpyDeviceToHost));
+
+        fprintf(stdout, "Matrix after COSQ: \n");
+        for(int i = 0; i < 8; i++) {
+            fprintf(stdout, "[");
+            for(int j = 8; j < 16; j++) {
+                fprintf(stdout, "%f ", matrix[i*image->width + j]);
+            }
+            fprintf(stdout, "]\n");
+        }
+
         checkCudaErrors(cudaFree(device_plane_src));
         checkCudaErrors(cudaFree(device_plane_result));
         free_image(image);
