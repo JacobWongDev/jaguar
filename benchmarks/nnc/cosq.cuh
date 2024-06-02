d9e1effed5c0014458ba95b6ed258aad6915de6f
JacobWongDev <19jsjw@queensu.ca>
2024-06-02 17:44:04 -0400

    Repaired errors - Reduction code in nnc_e32_v2 had an error involving the use of __shfl_down_sync - Added more code in NNC to compute summations for CC - There is a discrepancy between summations calculated on CPU vs. GPU. This causes test case to fail. Known issue, due to float point arithmetic.

--- benchmarks/nnc/cosq.cuh
+++ benchmarks/nnc/cosq.cuh
@@ -2,6 +2,11 @@
 #define WARP_SIZE 32
 #define FULL_MASK 0xffffffff
 
+/*
+    References
+    - Kahan Summation: https://graphics.stanford.edu/courses/cs205a-13-fall/assets/notes/chapter1.pdf
+*/
+
 /**
  * CUDA Kernel for the Nearest Neighbour Condition (nnc), for codebooks with greater than or equal to 32 elements.
  * - loads codebook into shared mem from global memory. Each thread loads <num_sums> elements
@@ -12,24 +17,31 @@
  * - Since this kernel is meant to be run on ONE WARP specifically, no __syncthreads() is required.
 */
 template <unsigned int levels>
-__global__ void nnc_e32(float* training_sequence, float* codebook, float* error_matrix, unsigned int* cells) {
+__global__ void nnc_e32(float* training_sequence, float* codebook, float* error_matrix, unsigned int* cells, float* cc_sums, unsigned int* cc_cardinal) {
     __shared__ float s_codebook[levels];
     __shared__ float s_sums[levels];
     __shared__ unsigned int s_min_indices[WARP_SIZE];
     unsigned int t = threadIdx.x;
     float target = training_sequence[blockIdx.x];
     float sum = 0;
+    float c = 0; // compensation
+    float next, next_sum;
     unsigned int num_sums = levels / WARP_SIZE;
     // load codebook into shared mem
     for(unsigned int k = 0; k < num_sums; k++) {
         s_codebook[num_sums*t + k] = codebook[num_sums*t + k];
     }
     for(unsigned int k = 0; k < num_sums; k++) {
+        // Kahan Summation
         for(unsigned int i = 0; i < levels; i++) {
-            sum += error_matrix[i + levels*(num_sums*t + k)] * (target - s_codebook[i]) * (target - s_codebook[i]);
+            next = error_matrix[i + levels*(num_sums*t + k)] * (target - s_codebook[i]) * (target - s_codebook[i]) + c;
+            next_sum = sum + next;
+            c = next - (next_sum - sum);
+            sum = next_sum;
         }
         s_sums[num_sums*t + k] = sum;
         sum = 0;
+        c = 0;
     }
     // Now, find minimum array INDEX of all the sums.
     unsigned int min_index = t;
@@ -64,6 +76,8 @@ __global__ void nnc_e32(float* training_sequence, float* codebook, float* error_
             s_min_indices[0] = s_min_indices[1];
         }
         cells[blockIdx.x] = s_min_indices[0];
+        atomicAdd(cc_sums + s_min_indices[0], target);
+        atomicAdd(cc_cardinal + s_min_indices[0], 1);
     }
 }
 
@@ -77,7 +91,7 @@ __global__ void nnc_e32(float* training_sequence, float* codebook, float* error_
  * - Since this kernel is meant to be run on ONE WARP specifically, no __syncthreads() is required.
 */
 template <unsigned int levels>
-__global__ void nnc_e32_v2(float* training_sequence, float* codebook, float* error_matrix, unsigned int* cells) {
+__global__ void nnc_e32_v2(float* training_sequence, float* codebook, float* error_matrix, unsigned int* cells, float* cc_sums, unsigned int* cc_cardinal) {
     __shared__ float s_codebook[levels];
     __shared__ float s_sums[levels];
     unsigned int t = threadIdx.x;
@@ -86,16 +100,23 @@ __global__ void nnc_e32_v2(float* training_sequence, float* codebook, float* err
     unsigned int num_sums = levels / WARP_SIZE;
     unsigned int min_index = t;
     unsigned int shfl_min_index;
+    float c = 0; // compensation
+    float next, next_sum;
     // load codebook into shared mem
     for(unsigned int k = 0; k < num_sums; k++) {
         s_codebook[num_sums*t + k] = codebook[num_sums*t + k];
     }
     for(unsigned int k = 0; k < num_sums; k++) {
+        // Kahan Summation
         for(unsigned int i = 0; i < levels; i++) {
-            sum += error_matrix[i + levels*(num_sums*t + k)] * (target - s_codebook[i]) * (target - s_codebook[i]);
+            next = error_matrix[i + levels*(num_sums*t + k)] * (target - s_codebook[i]) * (target - s_codebook[i]) + c;
+            next_sum = sum + next;
+            c = next - (next_sum - sum);
+            sum = next_sum;
         }
         s_sums[num_sums*t + k] = sum;
         sum = 0;
+        c = 0;
     }
     // Now, find minimum array INDEX of all the sums.
     for(unsigned int k = 1; k < num_sums; k++) {
@@ -103,19 +124,16 @@ __global__ void nnc_e32_v2(float* training_sequence, float* codebook, float* err
             min_index = WARP_SIZE*k + t;
         }
     }
-    shfl_min_index = min_index;
     #pragma unroll
     for(int offset = 16; offset > 0; offset /= 2) {
-        shfl_min_index = __shfl_down_sync(FULL_MASK, shfl_min_index, offset);
-        if(s_sums[min_index] < s_sums[shfl_min_index]) {
-            shfl_min_index = min_index;
+        shfl_min_index = __shfl_down_sync(FULL_MASK, min_index, offset);
+        if(s_sums[min_index] > s_sums[shfl_min_index]) {
+            min_index = shfl_min_index;
         }
     }
     if(t == 0) {
-        if(s_sums[min_index] < s_sums[shfl_min_index]) {
-            cells[blockIdx.x] = min_index;
-        } else {
-            cells[blockIdx.x] = shfl_min_index;
-        }
+        cells[blockIdx.x] = min_index;
+        atomicAdd(cc_sums + min_index, target);
+        atomicAdd(cc_cardinal + min_index, 1);
     }
 }
--- benchmarks/nnc/jaguar.cu
+++ benchmarks/nnc/jaguar.cu
@@ -1,12 +1,14 @@
 #include <random>
 #include <chrono>
+#include <iomanip>
 #include <iostream>
 #include "cosq.cuh"
 
 #define TRAINING_SIZE 1048576
 #define RATE 8
-#define POLYA_EPSILON 0
+#define POLYA_EPSILON 0.01
 #define POLYA_DELTA 0
+#define FLOAT_ERROR 1
 
 void check(cudaError_t error, const char* file, int line) {
     if(cudaSuccess != error) {
@@ -16,25 +18,36 @@ void check(cudaError_t error, const char* file, int line) {
 
 #define checkCudaErrors(error) check(error, __FILE__, __LINE__);
 
-unsigned int* nearest_neighbour(float* training_sequence, float* codebook, int levels, float* error_matrix, float* cell_sums) {
+unsigned int* nnc_cpu(float* training_sequence, float* codebook, int levels, float* error_matrix,
+                      float* cell_sums, float* cc_sums, unsigned int* cc_cardinality) {
   float min = __FLT_MAX__;
   unsigned int* cells = (unsigned int*) malloc(sizeof(unsigned int) * TRAINING_SIZE);
   int min_index = -1;
   float sum = 0;
+  float c = 0;
+  float next, next_sum;
   for(int i = 0; i < TRAINING_SIZE; i++) {
     float target = training_sequence[i];
     for(int l = 0; l < levels; l++) {
+      // Kahan summation
       for(int j = 0; j < levels; j++) {
-        sum += error_matrix[levels*l + j] * (target - codebook[j]) * (target - codebook[j]);
+        next = error_matrix[levels*l + j] * (target - codebook[j]) * (target - codebook[j]) + c;
+        next_sum = sum + next;
+        c = next - (next_sum - sum);
+        sum = next_sum;
       }
       if(sum < min) {
         min_index = l;
         min = sum;
       }
+      cell_sums[levels*i + l] = sum;
       sum=0;
+      c=0;
     }
     cells[i] = min_index;
-    cell_sums[i] = min;
+    // For Centroid Condition
+    cc_cardinality[min_index]++; // update count
+    cc_sums[min_index] += target; // running sum
     sum = 0;
     min_index = -1;
     min = __FLT_MAX__;
@@ -84,26 +97,88 @@ float* generate_normal_sequence() {
   float* normal_sequence = (float*) malloc(TRAINING_SIZE * sizeof(float));
   std::default_random_engine rng;
   rng.seed(31);
-  std::normal_distribution<float> distribution(0, 1);
+  std::normal_distribution<float> distribution(10, 1);
   for(int i = 0; i < TRAINING_SIZE; i++) {
       normal_sequence[i] = distribution(rng);
   }
   return normal_sequence;
 }
 
+void nnc_cc_test(unsigned int levels, float* training_sequence, unsigned int* cuda_cells,
+                 unsigned int* cuda_cc_cardinality, float* cuda_cc_training_sums) {
+  // First, take cpu cells and compute training sequence sums and count cardinality.
+  float cc_training_sums[levels] = {};
+  unsigned int cardinality[levels] = {};
+  unsigned int idx;
+  float c[levels] = {};
+  float next, next_sum;
+  float sum;
+  for(int i = 0; i < TRAINING_SIZE; i++) {
+    idx = cuda_cells[i];
+    cardinality[idx]++;
+    // Kahan summation on cc_training_sums[idx].
+    sum = cc_training_sums[idx];
+    next = training_sequence[i] + c[idx];
+    next_sum = sum + next;
+    c[idx] = next - (next_sum - sum);
+    cc_training_sums[idx] = next_sum;
+  }
+  bool equal = true;
+  std::cout << "Executing test cell cardinality and min sums..." << std::endl;
+  for(int i = 0; i < levels; i++) {
+    if((cuda_cc_cardinality[i] != cardinality[i]) || abs(cc_training_sums[i] - cuda_cc_training_sums[i]) > FLOAT_ERROR) {
+      std::cout << "Codebook element: " << i << " cuda nnc cardinality: " << cardinality[i] << " cuda cc cardinality: "
+                << cuda_cc_cardinality[i] << std::endl;
+      std::cout << "Codebook element: " << i << std::setprecision(10) << " cuda nnc sum: " << cc_training_sums[i]
+                << " cuda cc sum: " << cuda_cc_training_sums[i] << std::endl;
+      std::cout << "Test failed!" << std::endl;
+      equal = false;
+      break;
+    }
+  }
+  if(equal)
+    std::cout << "Test Passed!" << std::endl;
+}
+
+void nnc_cells_test(unsigned int levels, unsigned int* cuda_cells, unsigned int* cpu_cells, float* nnc_sums) {
+  bool equal = true;
+  std::cout << "Executing test on cells..." << std::endl;
+  for(int i = 0; i < TRAINING_SIZE; i++) {
+    if((cuda_cells[i] != cpu_cells[i]) && (nnc_sums[i*levels + cuda_cells[i]] != nnc_sums[i*levels + cpu_cells[i]])) {
+      std::cout << "Training element: " << i << " cpu: " << cpu_cells[i] << " cuda: " << cuda_cells[i] << std::endl;
+      std::cout << "Training element: " << i << std::setprecision(10) << " cpu min sum: " << nnc_sums[i*levels + cpu_cells[i]]
+                << " cuda min sum: " << nnc_sums[i*levels + cuda_cells[i]] << std::endl;
+      std::cout << "Test failed!" << std::endl;
+      equal = false;
+      break;
+    }
+  }
+  if(equal)
+    std::cout << "Test Passed!" << std::endl;
+}
+
 int main(int argc, char** argv) {
   const unsigned int levels = 1 << RATE;
-  float* normal_sequence = generate_normal_sequence();
+  float* training_sequence = generate_normal_sequence();
   float* error_matrix = compute_error_matrix(levels);
   float* codebook = (float*) malloc(sizeof(float) * levels);
-  float* cell_sums = (float*) malloc(sizeof(float) * TRAINING_SIZE); // can delete later, used for testing!
+  float* cc_training_sums = (float*) calloc(levels, sizeof(float));
+  unsigned int* cc_cardinality = (unsigned int*) calloc(levels, sizeof(unsigned int));
+  float* zero_cc_training_sums = (float*) calloc(levels, sizeof(float));
+  unsigned int* zero_cc_cardinality = (unsigned int*) calloc(levels, sizeof(unsigned int));
+  float* nnc_sums = (float*) malloc(sizeof(float) * levels * TRAINING_SIZE); // can delete later, used for testing!
+  // intialize codebook to first <levels> training samples
   for(int i = 0; i < levels; i++)
-      codebook[i] = normal_sequence[i];
-  // intialize codebook to first training samples
+    codebook[i] = training_sequence[i];
+
+  /*
+    Sequential NNC
+  */
   auto start = std::chrono::high_resolution_clock::now();
-  unsigned int* seq_cells = nearest_neighbour(normal_sequence, codebook, levels, error_matrix, cell_sums);
+  unsigned int* cpu_cells = nnc_cpu(training_sequence, codebook, levels, error_matrix, nnc_sums, cc_training_sums, cc_cardinality);
   auto end = std::chrono::high_resolution_clock::now();
   auto t = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  std::cout << ":::::::::::: Performance CPU-only code ::::::::::::" << std::endl;
   std::cout << "sequential result took " << t.count() << "ms." << std::endl;
 
   /*
@@ -112,82 +187,86 @@ int main(int argc, char** argv) {
   float* device_training_seq;
   float* device_error_matrix;
   float* device_codebook;
+  float* device_cc_training_sums;
+  unsigned int* device_cc_cardinality;
   unsigned int* device_cells;
   unsigned int* device_cells_2;
   checkCudaErrors(cudaMalloc((void **) &device_training_seq, TRAINING_SIZE*sizeof(float)));
   checkCudaErrors(cudaMalloc((void **) &device_error_matrix, levels*levels*sizeof(float)));
   checkCudaErrors(cudaMalloc((void **) &device_codebook, levels*sizeof(float)));
+  checkCudaErrors(cudaMalloc((void **) &device_cc_training_sums, levels*sizeof(float)));
+  checkCudaErrors(cudaMalloc((void **) &device_cc_cardinality, levels*sizeof(unsigned int)));
   checkCudaErrors(cudaMalloc((void **) &device_cells, TRAINING_SIZE*sizeof(float)));
   checkCudaErrors(cudaMalloc((void **) &device_cells_2, TRAINING_SIZE*sizeof(float)));
   unsigned int* cuda_cells = (unsigned int*) malloc(sizeof(unsigned int) * TRAINING_SIZE);
-  unsigned int* cuda_cells_2 = (unsigned int*) malloc(sizeof(unsigned int) * TRAINING_SIZE);
+  float* cuda_cc_training_sums = (float*) malloc(sizeof(float) * levels);
+  unsigned int* cuda_cc_cardinality = (unsigned int*) malloc(sizeof(unsigned int) * levels);
 
   /*
     Kernel nnc_e32
   */
   start = std::chrono::high_resolution_clock::now();
-  checkCudaErrors(cudaMemcpy(device_training_seq, normal_sequence, TRAINING_SIZE*sizeof(float), cudaMemcpyHostToDevice));
+  checkCudaErrors(cudaMemcpy(device_training_seq, training_sequence, TRAINING_SIZE*sizeof(float), cudaMemcpyHostToDevice));
   checkCudaErrors(cudaMemcpy(device_error_matrix, error_matrix, levels*levels*sizeof(float), cudaMemcpyHostToDevice));
   checkCudaErrors(cudaMemcpy(device_codebook, codebook, levels*sizeof(float), cudaMemcpyHostToDevice));
+  checkCudaErrors(cudaMemcpy(device_cc_training_sums, zero_cc_training_sums, levels*sizeof(float), cudaMemcpyHostToDevice));
+  checkCudaErrors(cudaMemcpy(device_cc_cardinality, zero_cc_cardinality, levels*sizeof(unsigned int), cudaMemcpyHostToDevice));
   dim3 grid_size = {TRAINING_SIZE, 1, 1};
   dim3 block_size = {32, 1, 1};
-  nnc_e32<levels><<<grid_size, block_size>>>(device_training_seq, device_codebook, device_error_matrix, device_cells);
+  nnc_e32<levels><<<grid_size, block_size>>>(device_training_seq, device_codebook, device_error_matrix,
+                                             device_cells, device_cc_training_sums, device_cc_cardinality);
   checkCudaErrors(cudaMemcpy(cuda_cells, device_cells, TRAINING_SIZE*sizeof(float), cudaMemcpyDeviceToHost));
+  checkCudaErrors(cudaMemcpy(cuda_cc_cardinality, device_cc_cardinality, levels*sizeof(unsigned int), cudaMemcpyDeviceToHost));
+  checkCudaErrors(cudaMemcpy(cuda_cc_training_sums, device_cc_training_sums, levels*sizeof(float), cudaMemcpyDeviceToHost));
   end = std::chrono::high_resolution_clock::now();
   t = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  std::cout << ":::::::::::: Performance nnc_e32 ::::::::::::" << std::endl;
   std::cout << "Kernel nnc_e32 result took " << t.count() << "ms." << std::endl;
+  std::cout << ":::::::::::: Tests for nnc_e32 ::::::::::::" << std::endl;
+  nnc_cells_test(levels, cuda_cells, cpu_cells, nnc_sums);
+  nnc_cc_test(levels, training_sequence, cuda_cells, cuda_cc_cardinality, cuda_cc_training_sums);
 
   /*
     Kernel nnc_e32_v2
   */
   start = std::chrono::high_resolution_clock::now();
-  checkCudaErrors(cudaMemcpy(device_training_seq, normal_sequence, TRAINING_SIZE*sizeof(float), cudaMemcpyHostToDevice));
+  checkCudaErrors(cudaMemcpy(device_training_seq, training_sequence, TRAINING_SIZE*sizeof(float), cudaMemcpyHostToDevice));
   checkCudaErrors(cudaMemcpy(device_error_matrix, error_matrix, levels*levels*sizeof(float), cudaMemcpyHostToDevice));
   checkCudaErrors(cudaMemcpy(device_codebook, codebook, levels*sizeof(float), cudaMemcpyHostToDevice));
+  checkCudaErrors(cudaMemcpy(device_cc_training_sums, zero_cc_training_sums, levels*sizeof(float), cudaMemcpyHostToDevice));
+  checkCudaErrors(cudaMemcpy(device_cc_cardinality, zero_cc_cardinality, levels*sizeof(unsigned int), cudaMemcpyHostToDevice));
   grid_size = {TRAINING_SIZE, 1, 1};
   block_size = {32, 1, 1};
-  nnc_e32_v2<levels><<<grid_size, block_size>>>(device_training_seq, device_codebook, device_error_matrix, device_cells_2);
-  checkCudaErrors(cudaMemcpy(cuda_cells_2, device_cells_2, TRAINING_SIZE*sizeof(float), cudaMemcpyDeviceToHost));
+  nnc_e32_v2<levels><<<grid_size, block_size>>>(device_training_seq, device_codebook, device_error_matrix,
+                                                device_cells_2, device_cc_training_sums, device_cc_cardinality);
+  checkCudaErrors(cudaMemcpy(cuda_cells, device_cells_2, TRAINING_SIZE*sizeof(float), cudaMemcpyDeviceToHost));
+  checkCudaErrors(cudaMemcpy(cuda_cc_cardinality, device_cc_cardinality, levels*sizeof(unsigned int), cudaMemcpyDeviceToHost));
+  checkCudaErrors(cudaMemcpy(cuda_cc_training_sums, device_cc_training_sums, levels*sizeof(float), cudaMemcpyDeviceToHost));
   end = std::chrono::high_resolution_clock::now();
   t = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+  std::cout << ":::::::::::: Performance nnc_e32_v2 ::::::::::::" << std::endl;
   std::cout << "Kernel nnc_e32_v2 result took " << t.count() << "ms." << std::endl;
-
-  // compare results
-  bool equal = true;
-  for(int i = 0; i < TRAINING_SIZE; i++) {
-    if((cuda_cells[i] != seq_cells[i]) && (cell_sums[cuda_cells[i]] != cell_sums[seq_cells[i]])) {
-      std::cout << "Iteration: " << i << " seq: " << seq_cells[i] << " cuda: " << cuda_cells[i] << std::endl;
-      std::cout << "Not equal! Test failed!" << std::endl;
-      equal = false;
-      break;
-    }
-  }
-  if(equal)
-    std::cout << "Equal! v1 Test Passed!" << std::endl;
-
-  // compare results
-  equal = true;
-  for(int i = 0; i < TRAINING_SIZE; i++) {
-    if((cuda_cells_2[i] != seq_cells[i]) && (cell_sums[cuda_cells_2[i]] != cell_sums[seq_cells[i]])) {
-      std::cout << "Iteration: " << i << " seq: " << seq_cells[i] << " cuda: " << cuda_cells_2[i] << std::endl;
-      std::cout << "Not equal! Test failed!" << std::endl;
-      equal = false;
-      break;
-    }
-  }
-  if(equal)
-    std::cout << "Equal! v2 Test Passed!" << std::endl;
+  std::cout << ":::::::::::: Tests for nnc_e32_v2 ::::::::::::" << std::endl;
+  nnc_cells_test(levels, cuda_cells, cpu_cells, nnc_sums);
+  nnc_cc_test(levels, training_sequence, cuda_cells, cuda_cc_cardinality, cuda_cc_training_sums);
 
   checkCudaErrors(cudaFree(device_training_seq));
   checkCudaErrors(cudaFree(device_error_matrix));
   checkCudaErrors(cudaFree(device_codebook));
+  checkCudaErrors(cudaFree(device_cc_cardinality));
+  checkCudaErrors(cudaFree(device_cc_training_sums));
   checkCudaErrors(cudaFree(device_cells));
   checkCudaErrors(cudaFree(device_cells_2));
-  free(seq_cells);
-  free(cell_sums);
+  free(cpu_cells);
+  free(nnc_sums);
+  free(zero_cc_cardinality);
+  free(zero_cc_training_sums);
+  free(cc_cardinality);
+  free(cc_training_sums);
+  free(cuda_cc_cardinality);
+  free(cuda_cc_training_sums);
   free(cuda_cells);
-  free(cuda_cells_2);
   free(codebook);
-  free(normal_sequence);
+  free(training_sequence);
   free(error_matrix);
 }
