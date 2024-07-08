8d92f359d347007c5b2a52b0a80e70a16ba2a3de
JacobWongDev <19jsjw@queensu.ca>
2024-07-08 14:40:12 -0400

    Updated accuracy tests - Accuracy test script updated for ease of use - new test results in tests/. Only tests up to bit rate 7 are included, since for any higher bit-rates testing take a very long time.

--- accuracy_test.sh
+++ accuracy_test.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-bit_rate=6
 rm -rf build
 cmake -S src/test/ -B build/ -DCMAKE_TOOLCHAIN_FILE=$(pwd)/vcpkg/scripts/buildsystems/vcpkg.cmake
 cd build
 make
 if [ $? -eq 0 ]; then
-    ./jaguar $bit_rate
+    for bit_rate in {1..10}
+    do
+        ./jaguar $bit_rate > ../br$bit_rate.txt
+    done
 fi
 cd ..
